b96b7f6db7d7eace9cc3d9eec91bfdae487262f9
Vlsarro <drekmaro@gmail.com>
2020-06-27 21:17:34 +0300

    Update example script

--- scripts/pybabel_spreadsheet.py
+++ scripts/pybabel_spreadsheet.py
@@ -2,7 +2,7 @@ import argparse
 import sys
 import logging
 
-from pybabel_utils.spreadsheets import PoFilesToSpreadsheet
+from pybabel_utils.spreadsheets import PoFilesToSpreadsheet, PoFilesFromSpreadsheet
 
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
@@ -18,6 +18,12 @@ def parse_args():
     parser_to_xlsx.add_argument('-o', '--output_file', nargs='?', const=1, default='po_as_table.xlsx',
                                 help='Name of the output file with format included')
 
+    parser_from_xlsx = subparsers.add_parser('from_xlsx')
+    parser_from_xlsx.add_argument('-i', '--input_spreadsheet', nargs=1,
+                                  help='Input .xlsx-file with translations (must contain msgids)')
+    parser_from_xlsx.add_argument('-d', '--input_folder', nargs=1, help='Folder with .po files')
+    parser_from_xlsx.add_argument('-o', '--output_dir', nargs=1, help='Folder to save updated .po files')
+
     if len(sys.argv[1:]) == 0:
         arg_parser.print_help()
         arg_parser.exit()
@@ -28,6 +34,8 @@ if __name__ == '__main__':
     parser, run_args = parse_args()
     if run_args.subcommand == 'to_xlsx':
         PoFilesToSpreadsheet.run(run_args.input_folder, run_args.output_file)
+    elif run_args.subcommand == 'from_xlsx':
+        PoFilesFromSpreadsheet.run(run_args.input_spreadsheet, run_args.input_folder, run_args.output_dir)
     else:
         parser.print_help()
         parser.exit()
